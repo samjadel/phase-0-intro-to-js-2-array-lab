c061782549887627546e676bd35827afccc14d52
samjadel <114316664+samjadel@users.noreply.github.com>
2022-10-23 02:35:46 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Write your solution here!
+// Write your solution here!
+var cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name){
+    cats.push(name);
+}
+function destructivelyPrependCat(name){
+    cats.unshift(name);
+}
+function destructivelyRemoveLastCat(){
+    cats.pop();
+}
+function destructivelyRemoveFirstCat(){
+    cats.shift();
+}
+function appendCat(name){
+     cats[cats.length] = name;
+}
